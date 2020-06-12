8e87030fe8ea91655a05669e7448f429eb08abcd
Anastasia Kalenik <Anastasia.Kalenik@thomsonreuters.com>
2020-06-12 15:18:50 +0300

    Updates

---
+++ precache-manifest.689c80c75d5599e17e5ec8d33e641dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69431960d4998a6f82000a6ea847ef02",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "bdbe882a6aecbce80f37",
+    "url": "/first-react-app/static/css/main.759ca820.chunk.css"
+  },
+  {
+    "revision": "602e1ac9913ea532afa9",
+    "url": "/first-react-app/static/js/2.845ad9f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.845ad9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbe882a6aecbce80f37",
+    "url": "/first-react-app/static/js/main.1f637fbb.chunk.js"
+  },
+  {
+    "revision": "2be46a5dcc512ec4fb89",
+    "url": "/first-react-app/static/js/runtime-main.1d703599.js"
+  }
+]);
